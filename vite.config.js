3304c676925e0b643ee9499b4d9e19fb21f4b56f
root <root@DESKTOP-CA7F5KS.localdomain>
2025-03-10 10:46:46 +0300

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // Replace with your actual GitHub repo name
+  base: "/anto-portfolio/", // Replace with your actual GitHub repo name
   plugins: [react()],
 })
